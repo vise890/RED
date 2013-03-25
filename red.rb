de3a17a3e5bb1d4d8f4058cd07b6e2efc0260590
Martino Visintin <vise890@gmail.com>
2013-03-25 11:23:18 +0000

    refactor file loading

--- red.rb
+++ red.rb
@@ -8,8 +8,7 @@ class RED < Thor
   desc "to_opr FILE", "convert .opr.toml FILE into an .opr usable by ESP-r"
   def to_opr(file)
     raise WrongFileType unless file.chomp.match /.opr.toml$/
-    content = File.read file
-    opr = TOML::Parser.new(content).parsed
+    opr = TOML.load_file file
     ap opr
   end
 end
