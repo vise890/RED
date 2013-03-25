a9ed7432f661bcd52c0a02ee7821d4c15166ecec
Martino Visintin <vise890@gmail.com>
2013-03-25 11:27:57 +0000

    refactor file loading

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'http://rubygems.org'
 
 gem 'toml', '~> 0.0.3'
+gem "activesupport", "~> 3.2.13"
 gem 'awesome_print'
 gem 'thor'
+
+
--- red.rb
+++ red.rb
@@ -8,8 +8,16 @@ class RED < Thor
   desc "to_opr FILE", "convert .opr.toml FILE into an .opr usable by ESP-r"
   def to_opr(file)
     raise WrongFileType unless file.chomp.match /.opr.toml$/
-    opr = TOML.load_file file
+    opr_hash = TOML.load_file file
+    
+    ap opr_hash
+    
+    opr = ""
+    opr += "*Operations 2.0\n"
+    opr += "*date " + Date.now
+    
     ap opr
+    
   end
 end
 
