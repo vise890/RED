34d5e05b1125f5678fd7ff296873e7c48eade1df
Martino Visintin <vise890@gmail.com>
2013-03-25 11:22:34 +0000

    add thor skeleton, begin toml parsing

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'http://rubygems.org'
 
 gem 'toml', '~> 0.0.3'
+gem 'awesome_print'
 gem 'thor'
--- red.rb
+++ red.rb
@@ -2,3 +2,16 @@ require 'rubygems'
 require 'bundler'
 Bundler.require(:default)
 
+class WrongFileType < StandardError; end
+
+class RED < Thor
+  desc "to_opr FILE", "convert .opr.toml FILE into an .opr usable by ESP-r"
+  def to_opr(file)
+    raise WrongFileType unless file.chomp.match /.opr.toml$/
+    content = File.read file
+    opr = TOML::Parser.new(content).parsed
+    ap opr
+  end
+end
+
+RED.start(ARGV)
